24af3d8ab9e616a24dd247b85af22fe9952df1e1
Nischal Subedi <nischal1337@gmail.com>
2025-12-27 07:18:42 +0545

    setup server

--- rest-api/src/index.ts
+++ rest-api/src/index.ts
@@ -1,1 +1,20 @@
-console.log("hello world");
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+
+
+const app = express()
+
+app.use(cors({credentials: true}))
+app.use(bodyParser.json())
+app.use(compression())
+app.use(cookieParser())
+
+const server = http.createServer(app)
+app.listen(8080,()=>{
+    console.log("Server running on http://localhost:8080/");
+    
+})
